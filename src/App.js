0e92339077bff08f38bd02059840b129a325d2f1
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-01-15 22:00:01 -0500

    App created and working functionally! Moving onto styling now.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
 import React from 'react';
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
+import './components/TodoComponents/Todo.css';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      inputText: ""
+    };
+  }
+
+  addTodo = event => {
+    event.preventDefault();
+    if (this.state.inputText) {
+      this.setState({
+        todos: [...this.state.todos, { task: this.state.inputText, id: Date.now(), completed: false }],
+        inputText: ""
+      });
+    }
+  };
+
+  handleInput = event => {
+    this.setState({
+      inputText: event.target.value
+    });
+  };
+
+  //.slice() to create a new array so you don't change the state
+  // Set todo equal to .map
+
+  todoComplete = id => {
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearCompleted = e => {
+    e.preventDefault();
+    let todos = this.state.todos.slice();
+    todos = todos.filter(todo => !todo.completed);
+    this.setState({ todos });
+  };
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todo4={this.state.todos}
+          handleComplete={this.todoComplete}
+        />
+        <TodoForm
+          addTodo={this.addTodo}
+          inputText={this.state.inputText}
+          handleInput={this.handleInput}
+          handleClearTodos={this.clearCompleted}
+        />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import "./Todo.css";
+
+function TodoForm(props) {
+    return (
+        <form>
+            <input
+                value={props.inputText}
+                onChange={props.handleInput}
+                type="text"
+                name="todo"
+                placeholder="What to do today?"
+            />
+            <button onClick={props.addTodo}>Add Todo</button>
+            <button onClick={props.handleClearTodos}>Clear Completed</button>
+        </form>
+    );
+}
+
+export default TodoForm;
